70579a3ecf0c5472f2a5e7e766c150b462181d61
Revalation <hentscheljr1@aol.com>
2015-04-07 16:08:07 -0400

    Create TCF_Messages.java

---
+++ src/com/tylerhyper/utils/core/TCF_Messages.java
@@ -0,0 +1,20 @@
+package com.tylerhyper.utils.core;
+
+import org.bukkit.ChatColor;
+
+public class TCF_Messages {
+   public static final String NO_PLAYER = ChatColor.RED + "That username cannot be found. Ensure your spelling is correct.";
+   public static final String NO_PREMS = ChatColor.DARK_RED + "You don't have the required permissions to execute this command.";
+   public static final String USAGE = ChatColor.GRAY + "The proper usage for this command is: ";
+   public static final String PROMOTE = ChatColor.GREEN + " Has been promoted to the rank";
+   public static final String SWEAR = ChatColor.GRAY + "Swearing is forbidden!";
+   public static final String CHILDREN_FRIENDLY_PLEASE = ChatColor.DARK_RED + "Hi there, we have seven year olds playing here- I'd love if you didn't say 'those type' of words.  Children friendly, please!";
+   public static final String FORUMS_URL = "http://tatsusurvival.boards.net";
+   public static final String TAG = ChatColor.WHITE + "[" + ChatColor.AQUA + "Tatsu" + ChatColor.GREEN + "Craft" + ChatColor.LIGHT_PURPLE + "Core" + ChatColor.WHITE + "] " + ChatColor.RESET;
+   public static final String TAG_MOD = ChatColor.WHITE + "[" + ChatColor.AQUA + "Tatsu" + ChatColor.GREEN + "Craft" + ChatColor.LIGHT_PURPLE + "Mod" + ChatColor.WHITE + "] " + ChatColor.RESET;
+   public static final String NOTCH = "<" + ChatColor.GOLD + "Notch" + ChatColor.WHITE + "> "; 
+   public static final String BOT_TAG = "<" + ChatColor.DARK_RED + "AdminBot" + ChatColor.WHITE + "> "; 
+ 
+   
+}
+// Written by zD12 | https://github.com/zd12
