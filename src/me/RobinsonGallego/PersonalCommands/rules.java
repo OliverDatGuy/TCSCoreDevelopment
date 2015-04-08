e20e4d1882b11487d70ccf0f0b087cfa8136229e
xdestroyer217 <xdestroyer217@xdestroyer217-OptiPlex-745>
2015-04-08 16:02:06 -0400

    Intergrate rules.java.

--- src/me/RobinsonGallego/PersonalCommands/rules.java
+++ src/me/RobinsonGallego/PersonalCommands/rules.java
@@ -1,1 +1,29 @@
+package me.RobinsonGallego.PersonalCommands;
 
+import org.TatsuCraft.Core.TatsuCraftCore;
+import net.pravian.bukkitlib.command.BukkitCommand;
+import net.pravian.bukkitlib.command.CommandPermissions;
+import net.pravian.bukkitlib.command.SourceType;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+@CommandPermissions(source = SourceType.PLAYER, permission = "")
+public class rules extends BukkitCommand<TatsuCraftCore>
+{
+    @Override
+    public boolean run(CommandSender sender, Command command, String commandLabel, String[] args)
+    {     
+        //I need the rules. @ImALuckyGuy
+        sender.sendMessage("");
+        sender.sendMessage("");
+        sender.sendMessage("");
+        sender.sendMessage("");
+        sender.sendMessage("");
+        sender.sendMessage("");
+        sender.sendMessage("");
+        sender.sendMessage("");
+        sender.sendMessage("");
+        sender.sendMessage("");
+        return true;
+    }
+}
